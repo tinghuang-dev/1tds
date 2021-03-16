8ba0fd759d64934faa2785a4cda56687d726685f
Jakeeeeeeeee <42314835+Jakeeeeeeeee@users.noreply.github.com>
2021-03-16 12:15:51 +1100

    Jakeyang/ch380/reset password template (#87)
    * feat(emailRestPassword): introduce emailResetPassword
    
    * refact(mail): refact sendResetPassword in mail
    
    * fix: fix file position
    
    * refact(resetPassword): change file name to resetPassword
    
    * feat(resetPassword): add reset-password link to reset password email
    
    Co-authored-by: Long Zhao <zlong@outlook.com>

--- src/lib/mail/mail.js
+++ src/lib/mail/mail.js
@@ -1,6 +1,7 @@
 import sgMail from '@sendgrid/mail';
 import parse from './parse';
 import emailVerification from './view/emailVerification.ejs';
+import resetPassword from './view/resetPassword.ejs';
 
 const {
   SENDGRID_API_KEY,
@@ -25,11 +26,15 @@ const sendEmailVerification = async (to, token) => {
   });
 };
 
-const sendResetPassword = (to, token) => send({
-  to,
-  subject: '[一团袋鼠]请确认你的邮箱',
-  text: '请点击重设你的密码',
-  html: `<a style="color: black;" href="http://localhost:3000/api/auth/reset-password?token=${token}">请点击链接重设你的密码</a>`,
-});
+const sendResetPassword = async (to, token) => {
+  const html = parse(resetPassword, { token });
+
+  return send({
+    to,
+    subject: '[一团袋鼠]请确认你的邮箱',
+    text: '请点击重设你的密码',
+    html,
+  });
+};
 
 export default { send, sendEmailVerification, sendResetPassword };
