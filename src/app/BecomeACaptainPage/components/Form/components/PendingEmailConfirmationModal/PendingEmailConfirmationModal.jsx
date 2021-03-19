9ddecb0c79143ba946bd454b8c41e4f8b63868b8
Simon Yu <31500670+SimonYu-Web@users.noreply.github.com>
2021-03-20 00:20:46 +1100

    fix(verifyemail): fix minor stuff (#103)

--- src/app/BecomeACaptainPage/components/Form/components/PendingEmailConfirmationModal/PendingEmailConfirmationModal.jsx
+++ src/app/BecomeACaptainPage/components/Form/components/PendingEmailConfirmationModal/PendingEmailConfirmationModal.jsx
@@ -33,7 +33,7 @@ export default function PendingEmailConfirmationModal({
       {httpRequestStatus && (
         <MessageBox variant={(httpRequestStatus !== 201) && 'error'}>
           {{
-            201: '验证信息发送成功',
+            201: '已重新发送邮件',
           }[httpRequestStatus] || '邮件发送失败，请稍后再试'}
         </MessageBox>
       )}
@@ -44,8 +44,10 @@ export default function PendingEmailConfirmationModal({
         lineHeight="loose"
         borderBottom="@1"
         borderBottomColor="border"
+        textAlign="center"
       >
-        {`验证邮箱已发送到你的邮箱 ${email},`}
+        验证邮件已发送到您的邮箱
+        <br />
         请点击邮件中的确认链接完成验证
       </Box>
 
@@ -55,9 +57,7 @@ export default function PendingEmailConfirmationModal({
 
       <Box fontSize="lg" my="md" lineHeight="loose">
         <li>
-          检查上面邮箱是否正确，
-          若错误请
-          {' '}
+          检查上面邮箱是否正确，若错误请
           <Button variant="link" onClick={onClose}>
             重新注册
           </Button>
