0a5bb5472247aa5ee0cc3953060b69442ca7fdcf
Long Zhao <zlong@outlook.com>
2021-03-21 15:18:48 +1100

    feat(RouterGuard): guard router from auth

--- src/controllers/auth/getUser/getUser.js
+++ src/controllers/auth/getUser/getUser.js
@@ -1,47 +1,19 @@
-import { verify } from 'jsonwebtoken';
-import Users from '../../../db/models/users';
+import { pipe } from 'ramda';
+import withAuth from '../../../middlewares/withAuth';
 
 const getUser = async (req, res) => {
-  const token = req.headers['x-auth-token'];
+  const { user } = req;
 
-  if (!token) {
-    res.status(400).end();
-    return;
-  }
+  const responseUser = {
+    id: user.id,
+    email: user.email,
+    mobile: user.mobile,
+    address: user.address,
+  };
 
-  const { JWT_SECRET } = process.env;
-
-  try {
-    const requestUser = verify(token, JWT_SECRET);
-
-    if (!requestUser) {
-      res.status(404).end();
-      return;
-    }
-
-    const user = await Users.findOne({ where: { id: requestUser.id } });
-
-    if (!user) {
-      res.status(401).end();
-      return;
-    }
-
-    const responseUser = {
-      id: user.id,
-      email: user.email,
-      mobile: user.mobile,
-      address: user.address,
-    };
-
-    res.status(200).json(responseUser);
-  } catch (e) {
-    if (e.name === 'TokenExpiredError') {
-      res.status(401).end();
-
-      return;
-    }
-
-    throw e;
-  }
+  res.status(200).json(responseUser);
 };
-export default getUser;
+
+export default pipe(
+  withAuth,
+)(getUser);
