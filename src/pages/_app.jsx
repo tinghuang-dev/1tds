06b1bb5256c36478a5ffe8a7714364d583ffa7f5
Jakeeeeeeeeeeeee <yangjinchen1995@gmail.com>
2021-04-08 00:00:54 +1000

    fix(_app): fix the conflict

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,21 +5,28 @@ import Header from '../app/Header';
 import '../styles/global.css';
 import '../styles/ZCOOLKuaile.css';
 import MessageProvider from '../app/MessageProvider';
+import Footer from '../app/Footer';
+import Box from '../components/Box';
+import Flex from '../components/Flex';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <ThemeProvider theme={theme}>
         <MessageProvider>
-          <Header />
-          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-          <Component {...pageProps} />
+          <Flex minHeight="100vh" flexDirection="column">
+            <Header />
+            <Box flex="1">
+              {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+              <Component {...pageProps} />
+            </Box>
+            <Footer height="180px" />
+          </Flex>
           <div id="modal-root" />
           <div id="message-root" />
         </MessageProvider>
       </ThemeProvider>
       <script
-        async
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDmb4QkQfvFvCYiiAyfSAJXfFBBn_4ty4g&&language=en&libraries=places"
       />
     </>
